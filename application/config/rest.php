44d0007708e13cf039c9f7ac6763755e372e59cb
qiaokun <qkbeyond@gmail.com>
2019-12-20 09:54:18 +0800

    禁用api-key

--- application/config/rest.php
+++ application/config/rest.php
@@ -329,8 +329,8 @@ $config['rest_keys_table'] = 'keys';
 |   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-//$config['rest_enable_keys'] = FALSE;
-$config['rest_enable_keys'] = TRUE;
+$config['rest_enable_keys'] = FALSE;
+//$config['rest_enable_keys'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
